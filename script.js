4d936b79447f6d8147820e4e515623aa17699c8a
nikeedev <nikita.gon.dev@gmail.com>
2024-03-12 09:49:04 +0100

    up

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+
 /// The root:
 const root = document.getElementById("root");
 
 ///
 
-const welcome = document.createElement("h1");
-welcome.innerText = "Welcome to NikOS!";
-welcome.id = "welcome-nikos";
-welcome.style.display = "none";
-root.appendChild(welcome);
+const welcome_nikos = document.createElement("h1");
+welcome_nikos.innerText = "Welcome to NikOS!";
+welcome_nikos.id = "welcome-nikos";
+welcome_nikos.style.display = "none";
+root.appendChild(welcome_nikos);
 
-const span = document.createElement("span");
-span.id = "nikos-version";
-span.innerText = "0.1.0a";
+const nikos_version = document.createElement("span");
+nikos_version.id = "nikos-version";
+nikos_version.innerText = "0.1.0a";
 const version = document.createElement("p");
 version.innerText = "NikOS - version ";
 version.id = "nikos-info";
-version.appendChild(span);
+version.appendChild(nikos_version);
 
 let onbutton = document.createElement("button");
 onbutton.id = "onbutton"
@@ -24,24 +25,33 @@ img.src = "assets/onbutton.svg";
 img.width = 24;
 img.style.color = "black";
 onbutton.appendChild(img);
-onbutton.onclick = OSStart;
+onbutton.onclick = () => OSStart(false);
 
 root.appendChild(onbutton);
 root.style.backgroundColor = "black";
 
 window.onkeydown = (e) => {
-    console.log(e)
     if (e.key == " ") {
-        OSStart();
+        OSStart(false);
+    } else if (e.key == "Enter" && e.shiftKey) {
+        OSStart(true)
     }
 }
-
-function OSStart() {
+function OSStart(_local) {
+    if (_local) {
+        root.style.transitionDuration = "0s";
+        root.style.transitionDelay = "0s";
+        nikos_version.style.transitionDuration = "0s";
+        nikos_version.style.transitionDelay = "0s";
+        welcome_nikos.style.transitionDuration = "0s";
+        welcome_nikos.style.transitionDelay = "0s";
+        welcome_nikos.style.display = "block";
+    }
     root.appendChild(version);
     onbutton.style.display = "none";
     root.style.backgroundColor = "white";
 
     root.ontransitionend = () => {
-        welcome.style.display = "block";
+        welcome_nikos.style.display = "block";
     }
 } 
